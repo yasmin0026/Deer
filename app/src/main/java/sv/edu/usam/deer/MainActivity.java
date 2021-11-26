f7b58dc1a45916b75170a56db0cda6c5f1f24dea
Kev <82066526+kevin200814@users.noreply.github.com>
2021-11-26 10:43:34 -0600

    Splash Activity Implementación

--- app/src/main/java/sv/edu/usam/deer/MainActivity.java
+++ app/src/main/java/sv/edu/usam/deer/MainActivity.java
@@ -2,27 +2,32 @@ package sv.edu.usam.deer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
     Button btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnMenu = findViewById(R.id.btnMenu);
-
-        btnMenu.setOnClickListener(new View.OnClickListener() {
+        TimerTask tarea = new TimerTask() {
             @Override
-            public void onClick(View v) {
-                Intent in = new Intent(getApplicationContext(), Menu.class);
-                startActivity(in);
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Menu.class);
+                startActivity(intent);
+                finish();
             }
-        });
+        };
 
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 5000);
     }
 }
