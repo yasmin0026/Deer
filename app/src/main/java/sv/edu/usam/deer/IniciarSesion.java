3338ad3345bf8fcb982d72f96620a52457b93ce2
yasmin0026 <rhina.espinoza201099@liveusam.edu.sv>
2021-12-02 21:46:32 -0600

    Login

---
+++ app/src/main/java/sv/edu/usam/deer/IniciarSesion.java
@@ -0,0 +1,87 @@
+package sv.edu.usam.deer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import sv.edu.usam.deer.ui.home.HomeFragment;
+
+public class IniciarSesion extends AppCompatActivity {
+    EditText usuario,password, id_rol;
+    Button btnLogin;
+    String id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_sesion);
+
+        usuario = findViewById(R.id.edtUsuario);
+        password = findViewById(R.id.edtPass);
+        id_rol = findViewById(R.id.edtId_rol);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loguearUser("https://deervideo2021.000webhostapp.com/crud/login.php?usuario="+usuario.getText().toString()+"&clave="+password.getText().toString());
+            }
+        });
+        /*---------------------------------------------------------------*/
+    }
+
+
+    private void loguearUser(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if(!response.isEmpty()){
+                    Intent intent = new Intent(getApplicationContext(), MenuNavegacion.class);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "Usuario o contraseña incorrecto", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("usuario",usuario.getText().toString());
+                parametros.put("clave",password.getText().toString());
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    /*-----------------------------------------------------------------*/
+}
--- app/src/main/java/sv/edu/usam/deer/MenuNavegacion.java
+++ app/src/main/java/sv/edu/usam/deer/MenuNavegacion.java
@@ -1,8 +1,10 @@
 package sv.edu.usam.deer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -15,6 +17,8 @@ import androidx.navigation.ui.NavigationUI;
 import sv.edu.usam.deer.databinding.ActivityMenuBinding;
 
 public class MenuNavegacion extends AppCompatActivity {
+
+    String id;
 
     private ActivityMenuBinding binding;
 
@@ -34,18 +38,59 @@ public class MenuNavegacion extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_menu);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+      /*  if(savedInstanceState == null){
+            Bundle extra = getIntent().getExtras();
+            if(extra == null){
+                Toast.makeText(this, "Sin rol",  Toast.LENGTH_LONG).show();
+            }else{
+                id = extra.getString("id_rol");
+            }
+
+        }else {
+            id = (String) savedInstanceState.getSerializable("id_rol");
+        }*/
+
+/*----------------------------------------------------------------*/
     }
 
     //Menu de opciones (no es el Navegation Bottom)
     public boolean onCreateOptionsMenu(Menu menu){
+
+       /* if(id.equals("1")){
+            getMenuInflater().inflate(R.menu.opciones_admin, menu);
+            return true;
+        }else if(id.equals("2")){
+            getMenuInflater().inflate(R.menu.opciones_usuarios, menu);
+            return true;
+
+        }*/
+
         getMenuInflater().inflate(R.menu.opciones, menu);
         return true;
+
     }
 
     //crear las opciones cada propiedad del menu
     public boolean onOptionsItemSelected(MenuItem item){
         //
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
+        int idMenu = item.getItemId();
+        switch(idMenu){
+            case R.id.iniciar_sesion:
+                viewSingIn();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        //return super.onOptionsItemSelected(item);
     }
+
+    public void viewSingIn(){
+        Intent intent = new Intent(getApplicationContext(), IniciarSesion.class);
+        startActivity(intent);
+    }
+
+
+/*------------------------------------------------------------------------------------*/
 }
